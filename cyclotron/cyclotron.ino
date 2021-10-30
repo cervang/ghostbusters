3a636bcceffafe3ef29850b4823c745750a02f98
cervang <gabriellejcervantes@gmail.com>
2021-10-30 00:24:24 -0700

    Adding base files

--- cyclotron/cyclotron.ino
+++ cyclotron/cyclotron.ino
@@ -4,14 +4,19 @@
 #endif
 #define PIN        6
 #define NUMPIXELS 16
-#define DELAYVAL_INIT 1000
+#define DELAYVAL_INIT 550
+#define STABLEDELAY 250
 #define LAST_DELAY 250
-#define ACTIVE_DURATION 14000
+#define FADE 4
+#define ACTIVE_DURATION 12000
 
 Adafruit_NeoPixel strip(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
 
 unsigned long startMillis;
 unsigned long currentMillis;
+
+uint32_t red = strip.Color(255,0,0);
+uint32_t green = strip.Color(0,255,0);
 
 void setup() {
 #if defined(__AVR_ATtiny85__) && (F_CPU == 16000000)
@@ -21,27 +26,31 @@ void setup() {
   //strip.show();
 }
 
+
 void loop() {
   int DELAYVAL = DELAYVAL_INIT;
   
   //Pulses speed up as they continue, stopping at a constant pace of 250
+  int loop = 0;
   for(double i = 1.0; i <= 5; i = i+.5){
     
     DELAYVAL = DELAYVAL/i;
-    if(DELAYVAL <= 250){
+    if(DELAYVAL <= STABLEDELAY){
        break;
     }
-    pixalSet(DELAYVAL);       
+    pixalSet(DELAYVAL, loop);   
+    loop++;    
   }
   
   currentMillis = millis();
   startMillis = millis();
   //this traps the LEDs in the same pace for the set amount of seconds
+  
   for(;;){
     if( (currentMillis - startMillis) >= (ACTIVE_DURATION) ){
       break;
     }
-    pixalSet(DELAYVAL);
+    pixalSet(DELAYVAL, loop);
     currentMillis = millis();
   }
 
@@ -49,9 +58,10 @@ void loop() {
   strip.clear();
   DELAYVAL = DELAYVAL_INIT;
 
-  for(int i = 0; i < 3; i++){
-    flashRed();
-    flashDark();
+  for(int i = 0; i < 2; i++){
+    flashDig();
+    //flashRed();
+    //flashDark();
     //strip.clear();  
   }
   strip.clear();
@@ -59,35 +69,28 @@ void loop() {
   
 }
 
-void pixalSet(int DELAYVAL){
-  strip.setPixelColor(0, strip.Color(250, 0, 0));
-  strip.setPixelColor(1, strip.Color(250, 0, 0));
-  strip.setPixelColor(2, strip.Color(250, 0, 0));
-  strip.setPixelColor(3, strip.Color(250, 0, 0));
-  strip.show();
-  delay(DELAYVAL-1);  
+void circleSet(int START, int END, int LOOP){
+  for(int i = 0; i <= 255; i++){
+    strip.fill(strip.Color(255-i,0,0), START, END);  
+    strip.show();
+    if(FADE-LOOP > 1){
+      delay( FADE-LOOP );  
+    }
+    
+  }  
+}
+
+void pixalSet(int DELAYVAL, int LOOP){
+  circleSet(0,4, LOOP);
   strip.clear();
-  strip.setPixelColor(4, strip.Color(250, 0, 0));
-  strip.setPixelColor(5, strip.Color(250, 0, 0));
-  strip.setPixelColor(6, strip.Color(250, 0, 0));
-  strip.setPixelColor(7, strip.Color(250, 0, 0));
-  strip.show();
-  delay(DELAYVAL);
+
+  circleSet(4,4, LOOP);
   strip.clear();
   
-  strip.setPixelColor(8, strip.Color(250, 0, 0));
-  strip.setPixelColor(9, strip.Color(250, 0, 0));
-  strip.setPixelColor(10, strip.Color(250, 0, 0));
-  strip.setPixelColor(11, strip.Color(250, 0, 0));
-  strip.show();
-  delay(DELAYVAL);
+  circleSet(8,4, LOOP);
   strip.clear();
-  strip.setPixelColor(12, strip.Color(250, 0, 0));
-  strip.setPixelColor(13, strip.Color(250, 0, 0));
-  strip.setPixelColor(14, strip.Color(250, 0, 0));
-  strip.setPixelColor(15, strip.Color(250, 0, 0));
-  strip.show();
-  delay(DELAYVAL);
+
+  circleSet(12,4, LOOP);
   strip.clear();
   
 }
@@ -97,6 +100,19 @@ void flashRed(){
   strip.fill(strip.Color(255,0,0));
   strip.show();
   delay(LAST_DELAY);
+}
+
+void flashDig(){
+  strip.fill(red, 0, 4); 
+  strip.fill(red, 8, 4); 
+  strip.show();
+  delay(LAST_DELAY);
+  flashDark();
+  strip.fill(red, 4, 4); 
+  strip.fill(red, 12, 4); 
+  strip.show();
+  delay(LAST_DELAY);
+  flashDark();
 }
 
 void flashDark(){
