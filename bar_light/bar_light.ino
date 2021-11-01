37bf34ebda6a3866daf7992b8d35dee3ed6697b7
cervang <gabriellejcervantes@gmail.com>
2021-11-01 13:22:19 -0700

    Changing the bar light to reflect change in circuit

--- bar_light/bar_light.ino
+++ bar_light/bar_light.ino
@@ -5,24 +5,18 @@
 #endif
 
 #define PIN_1 5 
-#define PIN_2 4
 #define NUMPIXELS 6
 //for the first set of NEO
 Adafruit_NeoPixel strip(NUMPIXELS, PIN_1, NEO_GRB + NEO_KHZ800);
-//second set of NEO
-Adafruit_NeoPixel strip_2(NUMPIXELS, PIN_2, NEO_GRB + NEO_KHZ800);
 uint32_t blue = strip.Color(0,0,255);
 
 void setup() {
   // put your setup code here, to run once:
   strip.begin();
-  strip_2.begin();
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  //flashDark();
-  //strip.clear();
   
   runGraph();
 }
@@ -30,15 +24,12 @@ void loop() {
 void runGraph(){
   for(int i = 0; i < 6; i++){
     strip.fill(blue, 0, i);
-    strip_2.fill(blue, 0, i);
     strip.show();
-    strip_2.show();
     delay(500);
     strip.clear();
-    strip_2.clear();
   }
   strip.clear();
-  strip_2.clear();
+
 }
 
 void flashDark(){
