920eceec751de66ea37180563f1412b2310ee447
cervang <gabriellejcervantes@gmail.com>
2021-11-01 13:22:42 -0700

    changing cyclo to clockwise

--- cyclotron/cyclotron.ino
+++ cyclotron/cyclotron.ino
@@ -2,6 +2,7 @@
 #ifdef __AVR__
   #include <avr/power.h>
 #endif
+
 #define PIN        6
 #define NUMPIXELS 16
 #define DELAYVAL_INIT 550
@@ -10,25 +11,33 @@
 #define FADE 4
 #define ACTIVE_DURATION 12000
 
-Adafruit_NeoPixel strip(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
+Adafruit_NeoPixel strip(NUMPIXELS, 6, NEO_GRB + NEO_KHZ800);
+//for the first set of NEO graph
+Adafruit_NeoPixel strip_graph_1(6, 4, NEO_GRB + NEO_KHZ800);
+//second set of NEO graph
+Adafruit_NeoPixel strip_graph_2(6, 5, NEO_GRB + NEO_KHZ800);
 
 unsigned long startMillis;
 unsigned long currentMillis;
 
 uint32_t red = strip.Color(255,0,0);
 uint32_t green = strip.Color(0,255,0);
+uint32_t blue = strip.Color(0,0,255);
 
 void setup() {
 #if defined(__AVR_ATtiny85__) && (F_CPU == 16000000)
   clock_prescale_set(clock_div_1);
 #endif
   strip.begin();
-  //strip.show();
+  strip_graph_1.begin();
+  strip_graph_2.begin();
 }
 
 
 void loop() {
   int DELAYVAL = DELAYVAL_INIT;
+  //run the graph
+  runGraph();
   
   //Pulses speed up as they continue, stopping at a constant pace of 250
   int loop = 0;
@@ -81,18 +90,19 @@ void circleSet(int START, int END, int LOOP){
 }
 
 void pixalSet(int DELAYVAL, int LOOP){
-  circleSet(0,4, LOOP);
+  circleSet(12,4, LOOP);
+  strip.clear();
+
+  circleSet(8,4, LOOP);
   strip.clear();
 
   circleSet(4,4, LOOP);
   strip.clear();
-  
-  circleSet(8,4, LOOP);
+
+  circleSet(0,4, LOOP);
   strip.clear();
 
-  circleSet(12,4, LOOP);
-  strip.clear();
-  
+ 
 }
 
 
@@ -113,6 +123,20 @@ void flashDig(){
   strip.show();
   delay(LAST_DELAY);
   flashDark();
+}
+
+void runGraph(){
+  for(int i = 0; i < 6; i++){
+    strip_graph_1.fill(blue, 0, i);
+    strip_graph_2.fill(blue, 0, i);
+    strip_graph_1.show();
+    strip_graph_2.show();
+    delay(500);
+    strip_graph_1.clear();
+    strip_graph_2.clear();
+  }
+  strip_graph_1.clear();
+  strip_graph_2.clear();
 }
 
 void flashDark(){
