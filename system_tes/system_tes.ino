bfa38ef4f9705b83e90f06d1a655ef810e77dabd
cervang <gabriellejcervantes@gmail.com>
2021-11-14 23:31:50 -0800

    Both functions happen at the same time now!

--- system_tes/system_tes.ino
+++ system_tes/system_tes.ino
@@ -12,19 +12,21 @@
 //for the first set of NEO
 Adafruit_NeoPixel graph(GRAPH_PIXEL, PIN_GRAPH, NEO_GRB + NEO_KHZ800);
 Adafruit_NeoPixel cyclo(CYCLO_PIXEL, PIN_CYCLO, NEO_GRB + NEO_KHZ800);
-uint32_t blue = graph.Color(0,0,255);
-uint32_t red = graph.Color(255,0,0);
-uint32_t dark = graph.Color(0,0,0);
-uint32_t green = graph.Color(0,255,0);
 
-int waitDown = 100;
-int cyclo_init_delay = 500;
+int bright = 255;
+uint32_t blue = graph.Color(0,0,bright);
+uint32_t red = graph.Color(bright,0,0);
+uint32_t dark = graph.Color(0,0,0);
+uint32_t green = graph.Color(0,bright,0);
+
 int runUp = 125;
-int set_up = false;
+int set_up = true;
 int start = 0;
 int graph_pixel_curr = 1;
+int j = 0;
+int fade_delay = 2;
 
-unsigned long eventTime_1 = 152; //the graph wait time 125
+unsigned long eventTime_1 = 100; //the graph wait time 125
 unsigned long eventTime_2 = 200; //the cyclo wait time 200
 
 unsigned long currTime = millis();
@@ -41,30 +43,60 @@ void setup() {
 void loop() {
   // put your main code here, to run repeatedly:
   currTime = millis();
-
   //so, every 125 for the bar graph, a light will be lit
   //then the graph will reset
   
   if(currTime - prevTime_1 > eventTime_1){
     if(set_up){
-        //run the 
+      //run the start up sequence 
+      
+      if(graph_pixel_curr > (GRAPH_PIXEL-j) ){  
+        
+        graph.clear();
+        graph.fill(blue, 0, graph_pixel_curr);
+        graph.show();
+        graph_pixel_curr++;
+        j = 0;
+        if(graph_pixel_curr > GRAPH_PIXEL){
+          graph_pixel_curr = 1;
+          set_up = false;
+          start_up(3);
+          start_up(2);
+          start_up(1);
+          start_up(1);
+        }
+      }else{
+        
+        graph.setPixelColor(GRAPH_PIXEL-j, blue);
+        graph.show();
+        graph.setPixelColor(GRAPH_PIXEL-j, dark);
+        j++;
+      
+      }   
+
     }else{
       //run steady state => run_up();
+      //graph_pixel_curr is the variable that keeps track of the pixel count
+      
       graph.fill(blue, 0, graph_pixel_curr);
       graph.show();
+      //if graph_pixel_curr is at the end of graph pixel count, reset to 1,
+      //else have graph_pixel_curr increase by one
       if(graph_pixel_curr >= GRAPH_PIXEL){
         graph_pixel_curr = 1;
       }else{
         graph_pixel_curr++;
       }
     }
+    //resets the time piece so that the process can run again
     prevTime_1 = currTime;
   }
-  graph.clear();
+  if(!set_up)graph.clear();
+  
   
   //at the same time, every cyclo will be fun in a for loop
-  if(currTime - prevTime_2 > eventTime_2){
-    start = fade_tube(start, 1);
+  if((currTime - prevTime_2 > eventTime_2) && !set_up){
+    start = fade_tube(start, fade_delay);
   }
   cyclo.clear();
   
@@ -72,55 +104,43 @@ void loop() {
 
 
 
-/* Fading the individual light tubes */
+/* Fading the individual light tubes 
+START : The index of the neopixal in the area
+FADE_DELAY : Delay of the gradient- used in forloop
+*/
 int fade_tube(int START, int FADE_DELAY){
+
+  //if START is greater than the pixel count, reset it to 0
   if(START >= 16){
     START = 0;
   }
+  
+  //this for loop creates a fade effect
   for(int i = 0; i <= 255; i++){
+    //cyclo.Color(255-i,0,0) -> Allows you to control the color brightness with forloop
+    //cyclo.fill(color, starting index, number of LEDs to fill from starting index);      
     cyclo.fill(cyclo.Color(255-i,0,0), START, 4);  
     cyclo.show();
+    //fade_delay is set to 1 when active, 4 when in startup
     delay(FADE_DELAY);
   }  
+  //increments the starting index
   return START + 4;
 }
 
-void runDown(int currPix){
-  for(int j = GRAPH_PIXEL; j >= currPix; j--){
-    graph.setPixelColor(j, blue);
-    graph.show();
-    delay(waitDown);
-    graph.setPixelColor(j, dark);
-  }
-}
 
-void run_up(){
-  
-  for(int i = 1; i <= GRAPH_PIXEL; i++){
-    graph.fill(blue, 0, i);
-    graph.show();
-    delay(runUp);
-    graph.clear();
-  }
-}
-
-void start_graph(){
-  /*lights are coded in parallel - this treats the code as the same in terms of receptors,
-   * meaning we only have to code one of the strips to get the same amount of code in both of them
-  */
-  for(int i = 1; i <= 6; i++){
-    runDown(i);
-    graph.fill(blue, 0, i);
-    graph.show();
-    delay(100);
-  }
+void start_up(int wait_time){
+  cyclo.clear();
   graph.clear();
-  set_up = false;
-  
-}
-
-void flashDark(){
-  cyclo.fill(cyclo.Color(0,0,0));
+  for(int k = 0; k < 255; k++){
+    cyclo.fill(graph.Color(255-k,0,0),0,16);
+    graph.fill(graph.Color(255-k,0,0),0,6);
+    cyclo.show();
+    graph.show();
+    delay(wait_time);
+  }
+  cyclo.fill(dark,0,16);
+  graph.fill(dark,0,6);
   cyclo.show();
-  delay(500);
+  graph.show();
 }
