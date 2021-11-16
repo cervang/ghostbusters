a85e4348008ecd950a07110a29493cf6a6f87424
cervang <gabriellejcervantes@gmail.com>
2021-11-15 20:14:21 -0800

    Adding logic for 5 led tubes

--- Rene_cyclo/Rene_cyclo.ino
+++ Rene_cyclo/Rene_cyclo.ino
@@ -1,127 +1,146 @@
 #include <Adafruit_NeoPixel.h>
+
 #ifdef __AVR__
-#include <avr/power.h>
+  #include <avr/power.h>
 #endif
 
-#define PIN        6
-#define NUMPIXELS 20
-#define DELAYVAL_INIT 550
-#define STABLEDELAY 250
-#define LAST_DELAY 250
-#define FADE 4
-#define ACTIVE_DURATION 12000
+#define PIN_GRAPH 4
+#define PIN_CYCLO 6 
+#define GRAPH_PIXEL 6
+#define CYCLO_PIXEL 16
 
-Adafruit_NeoPixel strip(NUMPIXELS, 6, NEO_GRB + NEO_KHZ800);
+//for the first set of NEO
+Adafruit_NeoPixel graph(GRAPH_PIXEL, PIN_GRAPH, NEO_GRB + NEO_KHZ800);
+Adafruit_NeoPixel cyclo(CYCLO_PIXEL, PIN_CYCLO, NEO_GRB + NEO_KHZ800);
 
+int bright = 255;
+uint32_t blue = graph.Color(0,0,bright);
+uint32_t red = graph.Color(bright,0,0);
+uint32_t dark = graph.Color(0,0,0);
+uint32_t green = graph.Color(0,bright,0);
 
-unsigned long startMillis;
-unsigned long currentMillis;
+int runUp = 125;
+int set_up = true;
+int start = 0;
+int graph_pixel_curr = 1;
+int j = 0;
+int fade_delay = 2;
 
-uint32_t red = strip.Color(255, 0, 0);
-uint32_t green = strip.Color(0, 255, 0);
-uint32_t blue = strip.Color(0, 0, 255);
+unsigned long eventTime_1 = 100; //the graph wait time 125
+unsigned long eventTime_2 = 200; //the cyclo wait time 200
+
+unsigned long currTime = millis();
+unsigned long prevTime_1 = millis();
+unsigned long prevTime_2 = millis();
 
 void setup() {
-#if defined(__AVR_ATtiny85__) && (F_CPU == 16000000)
-  clock_prescale_set(clock_div_1);
-#endif
-  strip.begin();
-
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  graph.begin();
+  cyclo.begin();
 }
-
 
 void loop() {
-  int DELAYVAL = DELAYVAL_INIT;
-
-  //Pulses speed up as they continue, stopping at a constant pace of 250
-  int loop = 0;
-  for (double i = 1.0; i <= 5; i = i + .5) {
-
-    DELAYVAL = DELAYVAL / i;
-    if (DELAYVAL <= STABLEDELAY) {
-      break;
-    }
-    pixalSet(DELAYVAL, loop);
-    loop++;
-  }
-
-  currentMillis = millis();
-  startMillis = millis();
-  //this traps the LEDs in the same pace for the set amount of seconds
-
-  for (;;) {
-    if ( (currentMillis - startMillis) >= (ACTIVE_DURATION) ) {
-      break;
-    }
-    pixalSet(DELAYVAL, loop);
-    currentMillis = millis();
-  }
-
-  //strip resets, delay_value resets, flashing red to signify end
-  strip.clear();
-  DELAYVAL = DELAYVAL_INIT;
-
-  for (int i = 0; i < 2; i++) {
-    flashDig();
-    //flashRed();
-    //flashDark();
-    //strip.clear();
-  }
-  strip.clear();
-  delay(2000);
-
-}
-
-void circleSet(int START, int END, int LOOP) {
-  for (int i = 0; i <= 255; i++) {
-    strip.fill(strip.Color(255 - i, 0, 0), START, END);
-    strip.show();
-    if (FADE - LOOP > 1) {
-      delay( FADE - LOOP );
-    }
-
-  }
-}
-
-void pixalSet(int DELAYVAL, int LOOP) {
-  //start at 0 , light up six
-  circleSet(0, 6, LOOP);
-  strip.clear();
-  //start at 6 light up 4
-  circleSet(6, 4, LOOP);
-  strip.clear();
-  //start at 10, light up 4
-  circleSet(10, 4, LOOP);
-  strip.clear();
+  // put your main code here, to run repeatedly:
+  currTime = millis();
+  //so, every 125 for the bar graph, a light will be lit
+  //then the graph will reset
   
-  circleSet(14, 6, LOOP);
-  strip.clear();
+  if(currTime - prevTime_1 > eventTime_1){
+    if(set_up){
+      //run the start up sequence 
+      
+      if(graph_pixel_curr > (GRAPH_PIXEL-j) ){  
+        
+        graph.clear();
+        graph.fill(blue, 0, graph_pixel_curr);
+        graph.show();
+        graph_pixel_curr++;
+        j = 0;
+        if(graph_pixel_curr > GRAPH_PIXEL){
+          graph_pixel_curr = 1;
+          set_up = false;
+          start_up(3);
+          start_up(2);
+          start_up(1);
+          start_up(1);
+        }
+      }else{
+        
+        graph.setPixelColor(GRAPH_PIXEL-j, blue);
+        graph.show();
+        graph.setPixelColor(GRAPH_PIXEL-j, dark);
+        j++;
+      
+      }   
+
+    }else{
+      //run steady state => run_up();
+      //graph_pixel_curr is the variable that keeps track of the pixel count
+      
+      graph.fill(blue, 0, graph_pixel_curr);
+      graph.show();
+      //if graph_pixel_curr is at the end of graph pixel count, reset to 1,
+      //else have graph_pixel_curr increase by one
+      if(graph_pixel_curr >= GRAPH_PIXEL){
+        graph_pixel_curr = 1;
+      }else{
+        graph_pixel_curr++;
+      }
+    }
+    //resets the time piece so that the process can run again
+    prevTime_1 = currTime;
+  }
+  if(!set_up)graph.clear();
+  
+  
+  //at the same time, every cyclo will be fun in a for loop
+  if((currTime - prevTime_2 > eventTime_2) && !set_up){
+    start = fade_tube(start, fade_delay);
+  }
+  cyclo.clear();
   
 }
 
 
-void flashRed() {
-  strip.fill(strip.Color(255, 0, 0));
-  strip.show();
-  delay(LAST_DELAY);
+
+/* Fading the individual light tubes 
+START : The index of the neopixal in the area
+FADE_DELAY : Delay of the gradient- used in forloop
+*/
+int fade_tube(int START, int FADE_DELAY){
+
+  //if START is greater than the pixel count, reset it to 0
+  if(START >= 20){
+    START = 0;
+  }
+  
+  //this for loop creates a fade effect
+  for(int i = 0; i <= 255; i++){
+    //cyclo.Color(255-i,0,0) -> Allows you to control the color brightness with forloop
+    //cyclo.fill(color, starting index, number of LEDs to fill from starting index);      
+    cyclo.fill(cyclo.Color(255-i,0,0), START, 5);  
+    cyclo.show();
+    //fade_delay is set to 1 when active, 4 when in startup
+    delay(FADE_DELAY);
+  }  
+  //increments the starting index
+  return START + 5;
 }
 
-void flashDig() {
-  strip.fill(red, 0, 4);
-  strip.fill(red, 8, 4);
-  strip.show();
-  delay(LAST_DELAY);
-  flashDark();
-  strip.fill(red, 4, 4);
-  strip.fill(red, 12, 4);
-  strip.show();
-  delay(LAST_DELAY);
-  flashDark();
-}
 
-
-void flashDark() {
-  strip.fill(strip.Color(0, 0, 0));
-  strip.show();
-  delay(LAST_DELAY);
+void start_up(int wait_time){
+  cyclo.clear();
+  graph.clear();
+  for(int k = 0; k < 255; k++){
+    cyclo.fill(graph.Color(255-k,0,0),0,16);
+    graph.fill(graph.Color(255-k,0,0),0,6);
+    cyclo.show();
+    graph.show();
+    delay(wait_time);
+  }
+  cyclo.fill(dark,0,16);
+  graph.fill(dark,0,6);
+  cyclo.show();
+  graph.show();
 }
